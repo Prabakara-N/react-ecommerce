cd1d85c82382d0bda11bf9d3575c505fc707449d
prabakaran <mspk028@gmail.com>
2023-03-17 12:22:53 +0530

    sidebar added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -14,8 +14,24 @@ import CartItem from "../components/CartItem";
 import { SidebarContext } from "../contexts/SidebarContext";
 
 const Sidebar = () => {
-  const { isOpen, setIsOpen } = useContext(SidebarContext);
-  return <div>Sidebar</div>;
+  const { isOpen, setIsOpen, handleClose } = useContext(SidebarContext);
+  return (
+    <div
+      className={`${
+        isOpen ? "right-0" : "-right-full"
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+    >
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="uppercase text-sm font-semibold">Shopping Bag (0)</div>
+        <div
+          onClick={handleClose}
+          className="cursor-pointer w-8 h-8 flex justify-center items-center"
+        >
+          <IoMdArrowForward className="text-2xl" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;

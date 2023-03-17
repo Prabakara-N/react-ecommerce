991e5910e3593a8a92e3570e87a0956cb8fcc75c
prabakaran <mspk028@gmail.com>
2023-03-17 11:13:56 +0530

    product context added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import "./styles/App.css";
-import "./styles/normalize.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// import pages
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+
+// import components
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-6xl">Tailwind installed</h1>
+    <div className="overflow-hidden">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Sidebar />
+        <Footer />
+      </Router>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// css
 import "./styles/App.css";
 import "./styles/normalize.css";
+// product provider
+import ProductProvider from "./contexts/ProductContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ProductProvider>
+      <App />
+    </ProductProvider>
   </React.StrictMode>
 );
